fbeb2eb20fdf939e017dc3c9d2b9ae328a7efa3d
punkouter26 <punkouter26@gmail.com>
2026-08-23 19:35:17 -0400

    ...

--- src/PoLocalCompare.Shared/DTOs/OllamaGpuStatusDto.cs
+++
@@ -1,8 +0,0 @@
-namespace PoLocalCompare.Shared.DTOs;
-
-public sealed class OllamaGpuStatusDto
-{
-    public string ModelName { get; init; } = string.Empty;
-    public bool IsGpu { get; init; }
-    public string? DeviceName { get; init; }
-}
